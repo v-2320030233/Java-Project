e7edf8f08fd37cbbd5a7dbd11d868ab6b1df8d4d
v-2320030233 <2320030233@klh.edu.in>
2024-09-10 13:16:40 +0530

    Add files via upload

---
+++ File System with TDD Approach/src/Directory.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Directory extends FileSystemEntity
+{
+    private List<FileSystemEntity> contents;
+    public Directory(String name)
+    {
+        super(name);
+        this.contents = new ArrayList<>();
+    }
+    public void add(FileSystemEntity entity)
+    {
+        contents.add(entity);
+    }
+    @Override
+    public void print(String indent)
+    {
+        System.out.println(indent + "Directory: " + name);
+        for (FileSystemEntity entity : contents)
+        {
+            entity.print(indent + "  ");
+        }
+    }
+    public List<FileSystemEntity> getContents()
+    {
+        return contents;
+    }
+}
